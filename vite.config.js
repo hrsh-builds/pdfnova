908af10739a3016ede788c0a174f4e751a068714
hrsh-builds <hrrsshhhh@gmail.com>
2026-03-22 18:54:01 +0530

    Use new docker backend URL

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
-export const API_URL = "https://pdfnova-backend-docker.onrender.com";
+export const API_URL = "https://pdfnova-docker.onrender.com/";
